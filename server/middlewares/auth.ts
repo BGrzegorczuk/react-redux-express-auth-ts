2fda88369186fd122faa9c74477e818d0b5047a2
Bartosz Grzegorczuk <bartosz.grzegorczuk@gmail.com>
2017-06-12 16:38:07 +0200

    [server-auth] `authorizationStrategy` implemented

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -1,7 +1,44 @@
 import * as passport from 'passport';
 import User, {IUserModel} from '../models/UserModel';
 import {JWT_SECRET} from '../config';
+import {
+    ExtractJwt,
+    Strategy as JwtStrategy,
+    StrategyOptions as IJwtStrategyOptions,
+    VerifiedCallback
+} from 'passport-jwt';
 import {IStrategyOptions as ILocalStrategyOptions, Strategy as LocalStrategy} from 'passport-local';
+
+
+// Authorization Strategy
+
+export interface IJwtStrategyPayload {
+    sub: string | number;
+    iat: number;
+}
+
+const authorizationStrategyOpts: IJwtStrategyOptions = {
+    secretOrKey: JWT_SECRET,
+    jwtFromRequest: ExtractJwt.fromHeader('authorization')
+};
+
+const authorizationStrategy: JwtStrategy = new JwtStrategy(
+    authorizationStrategyOpts, function(payload: IJwtStrategyPayload, done: VerifiedCallback) {
+
+        User.findById(payload.sub)
+            .then( (user: IUserModel) => {
+
+                if (user) {
+                    return done(null, user); // user found
+                } else {
+                    return done(null, false); // user not found in DB
+                }
+            })
+            .catch( (err: any) => done(err, null) );
+    });
+
+passport.use(authorizationStrategy);
+
 
 // Login Strategy
 
