fae44903abb5841c19adae1d40f7fe8f25087188
Bartosz Grzegorczuk <bartosz.grzegorczuk@gmail.com>
2017-06-12 22:08:32 +0200

    [server-auth] `App` container changed to a class using `Provider` component

--- client/ts/containers/App.tsx
+++ client/ts/containers/App.tsx
@@ -1,19 +1,39 @@
 import * as React from 'react';
+import {Store as IReduxStore} from 'redux';
+import {connect, Provider} from 'react-redux';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import LoginView from '../auth/views/LoginView';
 import SignupView from '../auth/views/SignupView';
 import NotFoundView from './NotFoundView';
 
-const App = () => (
-    <Router>
-        <main className="app">
-            <Switch>
-                <Route path="/login" exact component={LoginView}/>
-                <Route path="/signup" exact component={SignupView}/>
-                <Route path="/*" component={NotFoundView}/>
-            </Switch>
-        </main>
-    </Router>
-);
 
-export default App
+interface IStateProps {
+    isUserAuthorized: boolean;
+}
+
+interface IActionProps {}
+
+export interface IAppProps extends IStateProps, IActionProps {
+    store: IReduxStore<any>;
+}
+
+class App extends React.Component<IAppProps, {}> {
+    public render(): JSX.Element {
+        return (
+            <Provider store={this.props.store}>
+                <Router>
+                    <main className="app">
+                        <Switch>
+                            <Route path="/login" exact component={LoginView}/>
+                            <Route path="/signup" exact component={SignupView}/>
+                            <Route path="/*" component={NotFoundView}/>
+                        </Switch>
+                    </main>
+                </Router>
+            </Provider>
+        );
+    }
+}
+
+
+export default connect(undefined, {})(App);
