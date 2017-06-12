946e49f2717a0eb79e90f280e9ba482eca9ce2b2
Bartosz Grzegorczuk <bartosz.grzegorczuk@gmail.com>
2017-06-12 22:09:18 +0200

    [server-auth] basic `app reducer`

--- client/ts/index.tsx
+++ client/ts/index.tsx
@@ -1,13 +1,23 @@
+import {applyMiddleware, compose, createStore} from 'redux';
+import thunk from 'redux-thunk';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
-
 import App from './containers/App';
+import appReducer from './reducer';
+
+
+// configure store
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(appReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
+
 
 const render = (Component: any) => {
     ReactDOM.render(
         <AppContainer>
-            <Component />
+            <Component store={store}/>
         </AppContainer>,
         document.getElementById('root')
     )
