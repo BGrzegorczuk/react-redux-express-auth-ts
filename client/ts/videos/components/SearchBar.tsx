02226b0cdb387e9a24e9d9d34249f8d3be909fe0
Bartosz Grzegorczuk <bartosz.grzegorczuk@gmail.com>
2017-06-13 22:21:59 +0200

    [feature/videos] `SearchBarInput` & `SearchBar` components

---
+++ client/ts/videos/components/SearchBar.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import SearchInput from './SearchInput';
+
+interface ISearchBarProps {
+    onVideoSearch: (term: string) => void;
+}
+
+interface ISearchBarState {
+    input: string;
+}
+
+
+class SearchBar extends React.Component<ISearchBarProps, ISearchBarState> {
+
+    constructor(props: ISearchBarProps) {
+        super(props);
+
+        this.state = {
+            input: ""
+        }
+    }
+
+    // TODO: typings
+    private onInputChange = (e: React.FormEvent<any>) => {
+        console.log(e);
+        const target = (e as any).target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({ input: value });
+        this.props.onVideoSearch(value);
+    }
+
+    public render(): JSX.Element {
+        return (
+            <div className="search-bar">
+                <SearchInput
+                    id="search-bar-input"
+                    placeholder="Start typing to search..."
+                    onChange={this.onInputChange}
+                    value={this.state.input}
+                />
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
---
+++ client/ts/videos/components/SearchInput.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import {FormControl, FormGroup, HelpBlock} from 'react-bootstrap';
+
+interface InputProps {
+    id: string;
+    className?: string;
+    placeholder?: string;
+    help?: string;
+    value: string|number;
+
+    onChange?: React.FormEventHandler<SearchInput>;
+}
+
+interface InputState {}
+
+
+class SearchInput extends React.Component<InputProps, InputState> {
+
+    public render(): JSX.Element {
+        const { help, id, placeholder, onChange } = this.props;
+
+
+        return (
+            <FormGroup controlId={id}>
+                <FormControl bsSize="lg" onChange={onChange} placeholder={placeholder}/>
+                {help && <HelpBlock>{help}</HelpBlock>}
+            </FormGroup>
+        );
+    }
+}
+
+export default SearchInput;
