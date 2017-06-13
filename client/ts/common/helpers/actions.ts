99acc15bb5ccf9c48e062cccfbc214681d9857d5
Bartosz Grzegorczuk <bartosz.grzegorczuk@gmail.com>
2017-06-13 17:10:40 +0200

    [server-auth] `createRequestActionTypes` helper implemented

---
+++ client/ts/common/helpers/actions.ts
@@ -0,0 +1,11 @@
+export interface RequestActionTypes {
+    start: string;
+    success: string;
+    error: string;
+}
+
+export const createRequestActionTypes = (actionName: string): RequestActionTypes => ({
+    start: `${actionName}_REQUEST`,
+    success: `${actionName}_SUCCESS`,
+    error: `${actionName}_ERROR`,
+});
