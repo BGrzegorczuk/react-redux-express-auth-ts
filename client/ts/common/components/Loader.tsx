03666f794f7c97c354d683911b86fbec58c7aed4
Bartosz Grzegorczuk <bartosz.grzegorczuk@gmail.com>
2017-06-13 21:54:21 +0200

    [server_auth] `Loader` component implemented

---
+++ client/ts/common/components/Loader.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+export const Loader = () => {
+    return (
+        <div className="spinner-container">
+            <div className="spinner">
+                <div className="double-bounce1"></div>
+                <div className="double-bounce2"></div>
+            </div>
+        </div>
+    );
+};
