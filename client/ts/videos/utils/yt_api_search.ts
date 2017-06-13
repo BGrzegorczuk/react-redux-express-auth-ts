f30eb5e54806e05739b2ac5448014193ff76a623
Bartosz Grzegorczuk <bartosz.grzegorczuk@gmail.com>
2017-06-14 00:06:06 +0200

    [feature/videos] simple `ytAPI` util with typings - should be moved to separated package in the future

---
+++ client/ts/videos/utils/yt_api_search.ts
@@ -0,0 +1,21 @@
+import {ISearchArgs} from './yt_api_types';
+const ROOT_API_URL = 'https://www.googleapis.com/youtube/v3/search';
+export const APIKey = "AIzaSyAFXU0ZOBUe9slg1yCtJFSWP-oXJweY5CY";     // should not be public ;)
+
+const ytAPI = (params: ISearchArgs): Promise<Response> => {
+
+    if (!params.key) {
+        throw new Error('Youtube Search - key parameter required');
+    }
+
+    const _params = { ...params, part: 'snippet' };
+
+    const esc = encodeURIComponent;
+    const query = Object.keys(_params)
+        .map(k => esc(k) + '=' + esc((_params as any)[k]))
+        .join('&');
+
+    return fetch(`${ROOT_API_URL}?${query}`);
+};
+
+export default ytAPI;
---
+++ client/ts/videos/utils/yt_api_types.ts
@@ -0,0 +1,57 @@
+export interface ISearchArgs {
+    key: string;
+    term?: string;
+    maxResults?: number;
+}
+
+export interface IVideoResponse {
+    kind: string;
+    etag: string;
+    nextPageToken: string;
+    regionCode: string;
+    pageInfo: IVideoPageInfo;
+    items: IVideoItem[];
+}
+
+export interface IVideoItem {
+    etag: string;
+    id: IVideoId;
+    kind: string;
+    snippet: IVideoSnippet;
+}
+
+export interface IVideoId {
+    kind:string;
+    videoId:string;
+}
+
+export interface IVideoSnippet {
+    channelId: string;
+    channelTitle: string;
+    description: string;
+    liveBroadcastContent: string;
+    publishedAt: string;
+    thumbnails: IVideoThumbnails;
+    title: string;
+}
+
+export interface IVideoPageInfo {
+    totalResults: number;
+    resultsPerPage: number;
+}
+
+export interface IVideoThumbnails {
+    default: IThumbnail;
+    high: IThumbnail;
+    medium: IThumbnail;
+}
+
+export interface IThumbnail {
+    url: string;
+    width: number;
+    height: number;
+}
+
+export interface ISearch {
+    (searchArgs: ISearchArgs, callback: (data: IVideoItem[]) => any): Promise<any>;
+}
